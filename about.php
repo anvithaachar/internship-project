3f5d5f1b3323b37d983201386bdba26f5eea9816
anvithaachar <ranvitha86@gmail.com>
2023-06-26 13:59:01 +0530

    added navbar

--- about.php
+++ about.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Electronics Store</title>
+    <title>about</title>
     <?php include("./includes/header.php");?>
 </head>
 <body>
 
 
-<h1 class="textprimary">hello</h1>
+
 
 
 
